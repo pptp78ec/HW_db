1854af60207beef28b602f258f6e71a074b48a0c
sezindmitrii <sezi_nh18@student.itstep.org>
2023-06-28 21:01:40 +0300

    DB data worx part2

--- app/src/main/java/com/example/hw_db/databaseAccess/DBDataGetter.kt
+++ app/src/main/java/com/example/hw_db/databaseAccess/DBDataGetter.kt
@@ -3,7 +3,11 @@ package com.example.hw_db.databaseAccess
 import android.annotation.SuppressLint
 import android.database.Cursor
 import android.util.Log
+import com.example.hw_db.Model.Buylist
 import com.example.hw_db.Model.Product
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
 
 class DBDataGetter(private val DbHelper:DBHelper) {
 
@@ -39,7 +43,7 @@ class DBDataGetter(private val DbHelper:DBHelper) {
         val db = DbHelper.open()
         var cursor: Cursor? = null
         try {
-            cursor = db.rawQuery("""SELECT Product.name, Product.count, Type.label, Product.checked, Lists.name as listname FROM [Product] JOIN [Type] on Product.count_type = Type._id JOIN [Lists] on Product.list_id = Lists._id""", null)
+            cursor = db.rawQuery("""SELECT Product.name, Product.count, Type.label, Product.checked, Lists.name as listname FROM [Product] JOIN [Type] on Product.count_type = Type._id JOIN [Lists] on Product.list_id = Lists._id WHERE Lists._id = """ + listId, null)
             val list = mutableListOf<Product>()
             do {
 
@@ -62,5 +66,30 @@ class DBDataGetter(private val DbHelper:DBHelper) {
         }
         return mutableListOf()
     }
-
+    fun getBuyListsList(): MutableList<Buylist>{
+        val db = DbHelper.open()
+        var cursor: Cursor? = null
+        try {
+            cursor = db.rawQuery("""SELECT Lists.name, Lists.date, Lists.description FROM [Lists]""" , null)
+            val list = mutableListOf<Buylist>()
+            do {
+                list.add(Buylist(cursor.getString(0), LocalDateTime.ofInstant(Instant.ofEpochMilli(cursor.getLong(1)), ZoneId.systemDefault()).toLocalDate() , cursor.getString(2)))
+            }while (cursor.moveToNext())
+            db.close()
+            cursor.close()
+            return list
+        }
+        catch (ex:Exception){
+            Log.e("Error", ex.message.toString())
+        }
+        finally {
+            if(db.isOpen){
+                db.close()
+            }
+            if(cursor!=null && !cursor.isClosed){
+                cursor.close()
+            }
+        }
+        return mutableListOf()
+    }
 }
