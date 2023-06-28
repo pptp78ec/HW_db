d1187f8cd765cf147a2ed92c0864cdf49adc6b42
sezindmitrii <sezi_nh18@student.itstep.org>
2023-06-28 07:46:15 +0300

    Initial commit

---
+++ app/src/main/java/com/example/hw_db/CreateUpdateActivity.kt
@@ -0,0 +1,11 @@
+package com.example.hw_db
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateUpdateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_update)
+    }
+}
