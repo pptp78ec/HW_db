fdb16193cd990b1d1aa0d62794491e3927582965
pptp78ec <pptp78ec@gmail.com>
2023-06-29 12:29:42 +0300

    ListViewDataAdapters

---
+++ app/src/main/java/com/example/hw_db/DataAdapters/BuyListDataAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.hw_db.DataAdapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import com.example.hw_db.Model.Buylist
+import com.example.hw_db.R
+
+
+class BuyListDataAdapter(val context: Context, var resource: Int, var objects: MutableList<Buylist>) :
+    ArrayAdapter<Buylist>(context, resource, objects) {
+
+    private var inflater: LayoutInflater = LayoutInflater.from(context)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var localConVertView = convertView
+        val viewHolder: ViewHolder
+        if(localConVertView == null){
+            localConVertView = inflater.inflate(this.resource, parent, false)
+            viewHolder = ViewHolder(localConVertView)
+            localConVertView.tag = viewHolder
+        }
+        else viewHolder = localConVertView.tag as ViewHolder
+
+        val model = objects[position]
+        viewHolder.nameText?.text = model.name
+        viewHolder.dateText?.text = model.date.toString()
+        viewHolder.descText?.text = model.description
+
+        return  localConVertView!!
+    }
+
+
+    class ViewHolder {
+        var nameText: TextView? = null
+        var dateText: TextView? = null
+        var descText: TextView? = null
+        constructor(view: View){
+            this.nameText = view.findViewById(R.id.listitem_list_name)
+            this.dateText = view.findViewById(R.id.listitem_date)
+            this.descText = view.findViewById(R.id.listitem_desc)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/hw_db/DataAdapters/ProdListDataAdapter.kt
@@ -0,0 +1,53 @@
+package com.example.hw_db.DataAdapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.CheckBox
+import android.widget.TextView
+import com.example.hw_db.Model.Product
+import com.example.hw_db.R
+
+class ProdListDataAdapter(val context: Context, var resource: Int,  var objects: MutableList<Product>) :
+    ArrayAdapter<Product>(context, resource, objects) {
+        val inflater: LayoutInflater = LayoutInflater.from(context)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var localConvertView = convertView
+        val viewHolder: ViewHolder
+        if(localConvertView == null){
+            localConvertView = inflater.inflate(this.resource, parent, false)
+            viewHolder = ViewHolder(localConvertView)
+            localConvertView.tag = viewHolder
+        }
+        else viewHolder = localConvertView.tag as ViewHolder
+
+        val model = objects[position]
+
+        viewHolder.nameText?.text = model.name
+        viewHolder.countText?.text = model.count.toString()
+        viewHolder.counttypeText?.text = model.counttype
+        viewHolder.checkBox?.isChecked = model.checked
+        viewHolder.listnameText?.text = model.listname
+
+        return localConvertView!!
+    }
+
+    class ViewHolder {
+        var nameText: TextView? = null
+        var countText: TextView? = null
+        var counttypeText: TextView? = null
+        var checkBox: CheckBox? = null
+        var listnameText: TextView? = null
+
+        constructor(view: View){
+            this.nameText = view.findViewById(R.id.listitem_prodname)
+            this.countText = view.findViewById(R.id.listitem_count)
+            this.counttypeText = view.findViewById(R.id.listitem_counttype)
+            this.checkBox = view.findViewById(androidx.appcompat.R.id.checked)
+            this.checkBox = view.findViewById(R.id.listitem_list_name)
+        }
+    }
+}
